513756689a5d75511d0b4d864e44bcdf8c3005ca
Kallyas <kallyasmedia@gmail.com>
2021-06-26 19:59:29 +0300

    Update UI

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import './App.css';
 import Header from './components/Header/Header';
+import Section from './components/Header/Section/Section';
+import Main from './components/Main/Main';
+import Footer from './components/Footer/Footer'
 
 function App() {
     return (
         <>
         <Header />
+        <Section />
+        <Main />
+        <Footer />
         </>
     );
 }
