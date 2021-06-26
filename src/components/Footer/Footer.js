19ddc3b2e6591e2ef15daacb7d7e90e4c923ded2
Kallyas <kallyasmedia@gmail.com>
2021-06-26 19:58:59 +0300

    Add footer

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function Footer() {
+    return (
+        <footer className="u-align-center u-clearfix u-footer u-grey-80 u-footer" id="sec-3c77">
+        <div className="u-clearfix u-sheet u-sheet-1">
+            <p className="u-small-text u-text u-text-variant u-text-1">
+                &copy; { new Date().getFullYear()} All Rights reserved
+            </p>
+        </div>
+    </footer>
+    )
+}
+
+export default Footer
---
+++ src/components/Header/Section/Section.js
@@ -0,0 +1,112 @@
+function Section() {
+    return (
+      <section
+        className="u-align-left u-clearfix u-white u-section-2"
+        id="carousel_2e61"
+      >
+        <div className="u-clearfix u-gutter-0 u-layout-wrap u-layout-wrap-1">
+          <div className="u-gutter-0 u-layout">
+            <div className="u-layout-row">
+              <div
+                className="
+            u-align-left
+            u-container-style
+            u-layout-cell
+            u-left-cell
+            u-size-23
+            u-size-60-md
+            u-layout-cell-1
+          "
+              >
+                <div className="u-container-layout u-valign-middle u-container-layout-1">
+                  <h1 className="u-custom-font u-text u-text-palette-5-dark-3 u-text-1">
+                    Branding
+                  </h1>
+                  <h5 className="u-custom-font u-font-montserrat u-text u-text-2">
+                    A full service design studio
+                  </h5>
+                  <p className="u-text u-text-default u-text-3">
+                    We offer a wide range of professional tailor-made design
+                    solutions, from Brand design and Illustration to Websites and
+                    Infographics.
+                  </p>
+                  <p className="u-text u-text-4">
+                    Image from
+                    <a
+                      href="https://www.freepik.com/free-photos/woman"
+                      className="
+                  u-border-2
+                  u-border-palette-3-base
+                  u-btn
+                  u-button-style
+                  u-none
+                  u-text-body-color
+                  u-btn-1
+                "
+                    >
+                      Freepik
+                    </a>
+                  </p>
+                  <a
+                    href="/"
+                    className="
+                u-active-white
+                u-border-radius-0
+                u-btn
+                u-btn-rectangle
+                u-button-style
+                u-custom-font
+                u-font-raleway
+                u-hover-white
+                u-palette-3-base
+                u-btn-2
+              "
+                  >
+                    read more
+                  </a>
+                </div>
+              </div>
+              <div
+                className="
+            u-container-style
+            u-hidden-md
+            u-hidden-sm
+            u-hidden-xs
+            u-layout-cell
+            u-palette-3-base
+            u-size-4-lg
+            u-size-4-xl
+            u-size-6-sm
+            u-size-6-xs
+            u-size-60-md
+            u-layout-cell-2
+          "
+              >
+                <div className="u-container-layout u-container-layout-2" />
+              </div>
+              <div
+                className="
+            u-align-left
+            u-container-style
+            u-image
+            u-layout-cell
+            u-right-cell
+            u-size-31-sm
+            u-size-31-xs
+            u-size-33-lg
+            u-size-33-xl
+            u-size-60-md
+            u-image-1
+          "
+              >
+                <div className="u-container-layout u-valign-bottom u-container-layout-3" />
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    );
+  }
+  
+  export default Section;
+  
